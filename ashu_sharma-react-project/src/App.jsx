0e79c984f9f9226d8d32146b337c6e0496e45b29
ashu sharma <ashusharma10071999@gmail.com>
2024-06-19 10:52:56 -0400

    Trigger CI workflow

--- ashu_sharma-react-project/src/App.jsx
+++ ashu_sharma-react-project/src/App.jsx
@@ -2,8 +2,8 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-
 // some changes ashu sharma c0894214
+//new change
 
 function App() {
   const [count, setCount] = useState(0)
